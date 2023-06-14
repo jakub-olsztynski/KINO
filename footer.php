05f95faf70ff3eca181e4e94d7a057d6d5cdf612
Jan Dyndor <dyndorjan@gmail.com>
2023-06-14 21:11:42 +0200

    mapka

--- footer.php
+++ footer.php
@@ -32,8 +32,26 @@
 						<a href="#"><img src="images/tw.png" alt=""/></a>
 						<a href="#"><img src="images/dribble.png" alt=""/></a>
 						<a href="#"><img src="images/pinterest.png" alt=""/></a>
+						
 					</div>
 				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<div class=social>
+					<iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36026.86105010261!2d86.89636727392538!3d27.991658608874886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e854a215bd9ebd%3A0x576dcf806abbab2!2sMt%20Everest!5e0!3m2!1sen!2spl!4v1684358366489!5m2!1sen!2spl"
+            width="400"
+            height="300"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            class="map"
+          ></iframe>
+						
+					</div>
+				</div>
+				<div class="mapa">
+       
 				<div class="clear"></div>
 			</div>
 		</div>
@@ -65,6 +83,9 @@
 #button111:hover  {
                 background-color:#b70929;
                 color:white;
+}
+.map{
+	width:100%;
 }
 
 </style>
